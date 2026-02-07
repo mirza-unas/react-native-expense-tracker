6ce2fe70ae9b7fe34d73d41bc2549b5edf20a131
mirza-unas <unasmirza316@gmail.com>
2026-02-07 18:40:35 +0500

    add delete transaction and transaction summary api

--- backend/controllers/transactionsController.js
+++ backend/controllers/transactionsController.js
@@ -23,10 +23,10 @@ export const createTransaction = async (req, res) => {
 
 export const getTransactionsByUserId = async (req, res) => {
   try {
-    const { user_id } = req.params;
+    const { userId } = req.params;
 
     const transactions =
-      await sql`SELECT * FROM transactions WHERE user_id = ${user_id} ORDER BY created_at DESC`;
+      await sql`SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`;
 
     res.status(200).json(transactions);
   } catch (error) {
@@ -34,3 +34,54 @@ export const getTransactionsByUserId = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+export const deleteTransactions  = async(req, res)=> {
+  try {
+    const { id} = req.params
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction ID" });
+    }
+
+    const result = await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`
+
+    if(result.length === 0){
+       return res.status(404).json({ message: "Transaction not found" });
+    }
+ res.status(200).json({ message: "Transaction deleted successfully" });
+
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export const getSummaryByUserId = async(req, res)=>{
+  try {
+    const {userId} = req.params
+
+      const balanceResult = await sql`
+      SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+    `;
+
+    const incomeResult = await sql`
+      SELECT COALESCE(SUM(amount), 0) as income FROM transactions
+      WHERE user_id = ${userId} AND amount > 0
+    `;
+
+    const expensesResult = await sql`
+      SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions
+      WHERE user_id = ${userId} AND amount < 0
+    `;
+
+    res.status(200).json({
+      balance: balanceResult[0].balance,
+      income: incomeResult[0].income,
+      expenses: expensesResult[0].expenses,
+    });
+  } catch (error) {
+      console.log("Error gettin the summary", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- backend/routes/transactionsRoute.js
+++ backend/routes/transactionsRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createTransaction } from "../controllers/transactionsController.js";
+import { createTransaction, deleteTransactions, getSummaryByUserId, getTransactionsByUserId } from "../controllers/transactionsController.js";
 
 const router = express.Router();
 
 router.post("/", createTransaction);
+router.get("/:userId", getTransactionsByUserId);
+router.delete("/:id", deleteTransactions);
+router.get("/summary/:userId", getSummaryByUserId);
 
 export default router;
