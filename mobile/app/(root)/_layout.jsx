8e44555c5ac2436aa8028e4a5a64726705266d51
mirza-unas <unasmirza316@gmail.com>
2026-03-02 22:52:04 +0500

    enhance authentication layout by adding loading state handling

--- mobile/app/(root)/_layout.jsx
+++ mobile/app/(root)/_layout.jsx
@@ -1,13 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useUser } from '@clerk/clerk-expo'
-import { Redirect, Stack } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+import { Redirect, Stack } from "expo-router";
 
 const Layout = () => {
-    const {isSignedIn} = useUser()
+  const { isSignedIn, isLoaded } = useUser();
+  if (!isLoaded) return null;
+  if (!isSignedIn) return <Redirect href={"/sign-in"} />;
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
 
-    if(!isSignedIn) return <Redirect href={'/sign-up'}/>
-  return <Stack screenOptions={{headerShown: false}}/>
-}
-
-export default Layout
+export default Layout;
