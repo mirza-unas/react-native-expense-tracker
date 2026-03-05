6fadab6e6d090a6f95ae934cef40b3fa00adccf9
mirza-unas <unasmirza316@gmail.com>
2026-03-05 22:18:50 +0500

    update layout component to include StatusBar

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,16 +1,18 @@
 import { Slot } from "expo-router";
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 import React from "react";
-import SafeScreen from '@/components/SafeScreen'
-import { ClerkProvider } from '@clerk/clerk-expo'
+import SafeScreen from "@/components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-  <SafeScreen >
-    <Slot screenOptions={{headerShown: false}} />
-  </SafeScreen>
-  </ClerkProvider>
-)
+      <SafeScreen>
+        <Slot screenOptions={{ headerShown: false }} />
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </ClerkProvider>
+  );
 }
